1789d863097391dd4dd0b19085a8cec56a5f488e
chauncey <chaunceyzhong@outlook.com>
2018-01-16 10:36:18 +0800

    添加用户管理跟角色管理功能.

--- src/junit/TestHibernate.java
+++ src/junit/TestHibernate.java
@@ -1,15 +1,14 @@
 package junit;
 
 
-import java.util.Date;
 
+import cn.itcast.elec.domain.ElecRole;
+import cn.itcast.elec.domain.ElecUser;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.Test;
-
-import cn.itcast.elec.domain.ElecText;
 
 public class TestHibernate {
 
@@ -22,13 +21,14 @@ public class TestHibernate {
 		SessionFactory sf = configuration.buildSessionFactory();
 		Session s = sf.openSession();
 		Transaction tr = s.beginTransaction();
-		
-		ElecText elecText = new ElecText();
-		elecText.setTextName("测试Hibernate名称");
-		elecText.setTextDate(new Date());
-		elecText.setTextRemark("测试Hibernate备注");
-		s.save(elecText);
-		
+
+		ElecUser user = new ElecUser();
+		user.setUserName("张三");
+		ElecRole role = new ElecRole();
+		role.setRoleID("100");
+		role.setRoleName("老师");
+        user.getElecRoles().add(role);
+        s.save(user);
 		tr.commit();
 		s.close();
 		
